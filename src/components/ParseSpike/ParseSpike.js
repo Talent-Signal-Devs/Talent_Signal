f68131059f0d9800fbbe87a3e0a556b81cc987f6
mnpkoerner <mnpkoerner@gmail.com>
2021-03-05 12:06:42 -0600

    autoPackage function running, both test functions triggering when desired

--- src/components/ParseSpike/ParseSpike.js
+++ src/components/ParseSpike/ParseSpike.js
@@ -6,27 +6,37 @@ import Papa from 'papaparse'
 export default function ParseSpike() {
 
     //state to hold file
+    //isReady to conditionally render submit button
     const [parse, setParse] = useState('');
     const [isReady, setIsReady] = useState(false)
 
+    //parses CSV and sets state to store value of parsed data
     function parseFile(event) {
         Papa.parse(event.target.files[0], {
             complete: function (results) {
-                console.log(results)
+                console.log(results.data)
                 setParse(results.data);
                 setIsReady(true)
             }
         })
 
     }
-    console.log('log outside of parseFile', parse)
 
 
-
+//I'm intending this function to package up and send the data to a saga/reducer
+//have it running on a conditionally rendered button to deal withy async timing of setting state varaible
+//
     function packageData(parsedData) {
             console.log('in package with data', parsedData)
-            console.log('done parsing')
     }
+
+//could also run function automatically
+    function autoPackage(ready){
+        if(ready){
+        console.log('in autoPackage', parse)
+        }
+    }
+    autoPackage(isReady)
 
     return (
         <>

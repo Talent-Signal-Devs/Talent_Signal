d5790e7fd3900df380aed58b5c4ebd0a75472cbd
Joshua Vedane <jvedane@gmail.com>
2021-03-05 09:20:58 -0600

    changed root reducer file

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import pets from './pet.reducer'
+
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +12,7 @@ import pets from './pet.reducer'
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  pets, // holds all the user's pets
+
 });
 
 export default rootReducer;

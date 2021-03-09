1d3e94f6dcf16a8f6f64e74f5fcea4ef4cfd0ba6
Simeon Willard <swillard4597@gmail.com>
2021-03-09 09:37:54 -0600

    updated styles on admin client page

--- src/components/AdminClientList/AdminClientList.jsx
+++ src/components/AdminClientList/AdminClientList.jsx
@@ -7,45 +7,77 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import ToolTip from '@material-ui/core/ToolTip';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles(() => ({
+  tableRow: {
+    backgroundColor: 'aliceblue',
+    '&:hover': {
+      opacity: '80%',
+      backgroundColor: 'lightgray'
+    }
+  },
+  table: {
+    minWidth: '100px',
+    width: '1200px',
+    margin: 'auto',
+    textAlign: 'center',
+    borderCollapse: 'collapse',
+  },
+}))
 
 function AdminClientList(props) {
 
+  const classes = useStyles();
   const clients = useSelector((store) => store.adminClientReducer);
   const [heading, setHeading] = useState('Admin Client List');
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch({ type: 'FETCH_ADMIN_CLIENTS' });
-  })
+  }, []);
+
+  const handleRowClick = () => {
+    // history.push('/clientDetails');
+  }
 
   return (
     <div>
       <div>
         <h2>{heading}</h2>
       </div>
+      <br />
+      <br />
       <div>
-        <TableContainer component={Paper} />
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Contract ID</TableCell>
-              <TableCell>Coach</TableCell>
-              <TableCell>Coaching Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {clients.map((client) => (
-              <TableRow key={client.id}>
-                <TableCell>{client.first_name} {client.last_name}</TableCell>
-                <TableCell>{client.contract_id}</TableCell>
-                <TableCell>{client.coach_first_name} {client.coach_last_name}</TableCell>
-                <TableCell>{client.status}</TableCell>
+        <TableContainer component={Paper} className={classes.table}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Contract ID</TableCell>
+                <TableCell>Coach</TableCell>
+                <TableCell>Coaching Status</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {clients.map((client) => (
+
+                <TableRow key={client.id} onClick={handleRowClick} className={classes.tableRow}>
+                  <TableCell>{client.first_name} {client.last_name}</TableCell>
+                  <TableCell>{client.contract_id}</TableCell>
+                  <TableCell>{client.coach_first_name} {client.coach_last_name}</TableCell>
+                  <TableCell>{client.status}</TableCell>
+                </TableRow>
+
+
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );

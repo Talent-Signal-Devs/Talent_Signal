18b2e29257efe7a901526c9358b28c1216738369
mnpkoerner <mnpkoerner@gmail.com>
2021-03-09 19:02:24 -0600

    prompt box allows for custom check input

--- src/components/AdminPayouts/AdminPayouts.jsx
+++ src/components/AdminPayouts/AdminPayouts.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
-import {useDispatch} from 'react-redux'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux'
 import ParseSpike from '../ParseSpike/ParseSpike'
 
 
 function AdminPayouts() {
 
   //for now, this is our confirmation counter, just something to give us a "unique" number
-  let confirmationNumber = 0
+
 
   const dispatch = useDispatch()
 
@@ -16,21 +16,24 @@ function AdminPayouts() {
 
   //function that runs on button press. I want this to run after the CSV uploads, but it will require some async await stuff
   function dispatchGet() {
-    dispatch({type: 'GET_PAYMENT'})
+    dispatch({ type: 'GET_PAYMENT' })
     console.log(payout)
   }
 
   //paynow packages together all necessary info to be sent to the server when ted pays
   function payNow(userID, clientArray) {
     const newDate = new Date()
-    const payout = {
-      user_id: userID,
-      clients: clientArray,
-      payout_date: newDate.toISOString(),
-      confirmation_number: confirmationNumber++
+    const confirmationNumber = prompt('enter check number')
+    if (confirmationNumber) {
+      const payout = {
+        user_id: userID,
+        clients: clientArray,
+        payout_date: newDate.toISOString(),
+        confirmation_number: confirmationNumber
+      }
+      console.log(payout)
+    dispatch({ type: 'PAY_COACH', payload: payout })
     }
-    console.log(payout)
-    dispatch({type: 'PAY_COACH', payload: payout})
   }
 
 
@@ -41,23 +44,23 @@ function AdminPayouts() {
 
       <h2>MADE IT</h2>
       <ParseSpike />
-      <button onClick={()=>dispatchGet()}>GET STUFF</button>
+      <button onClick={() => dispatchGet()}>GET STUFF</button>
       <table>
         <thead>
           <tr>
 
-              <td>Coach</td>
-              <td>Amount Owed</td>
-              <td>Pay?</td>
+            <td>Coach</td>
+            <td>Amount Owed</td>
+            <td>Pay?</td>
           </tr>
         </thead>
         <tbody>
-          {payout.map((debt)=>{
-            return(
+          {payout.map((debt) => {
+            return (
               <tr key={debt.user_id}>
-              <td>{debt.full_name}</td>
-              <td>{debt.total_owed}</td>
-              <td><button onClick={()=>payNow(debt.user_id, debt.clients)}>PAY NOW</button></td>
+                <td>{debt.full_name}</td>
+                <td>{debt.total_owed}</td>
+                <td><button onClick={() => payNow(debt.user_id, debt.clients)}>PAY NOW</button></td>
               </tr>
             )
           })}
--- src/redux/sagas/csv.saga.js
+++ src/redux/sagas/csv.saga.js
@@ -6,9 +6,6 @@ function* csvSubmitManual(action) {
   try {
     console.log('in csvSumbitManual with', action.payload)
     yield axios.post('/api/csv', action.payload);
-    //JORDAN!! right now, this doesn't call because we don't get a response from the server
-    //I think we'll need to use some async await stuff. But, once we send back a server response
-    //this put will trigger automatically. for now, it will have to be triggered manually
     yield put({type: 'GET_PAYMENT'})
 
 

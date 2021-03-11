131c908458a7be41a690aceea0bb3ee91c1b43b1
Joshua Vedane <jvedane@gmail.com>
2021-03-11 13:37:28 -0600

    table receives and displays data

--- src/components/CoachPayoutDetails/CoachPayoutDetails.jsx
+++ src/components/CoachPayoutDetails/CoachPayoutDetails.jsx
@@ -2,13 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { TextField, Button } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(() => ({
+  tableRow: {
+    backgroundColor: 'aliceblue',
+    '&:hover': {
+      opacity: '80%',
+      backgroundColor: 'lightblue'
+    }
+  },
+  table: {
+    minWidth: '100px',
+    width: '90vw',
+    margin: 'auto',
+    textAlign: 'center',
+    borderCollapse: 'collapse',
+  },
+  dashContainer: {
+    display:"flex",
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'space-between'
+  }
+}))
 
 function CoachPayoutDetails(props) {
   const page = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
+  const classes = useStyles();
 
-  const store = useSelector((store) => store);
+  const payments = useSelector((store) => store.coachPaymentDetailsReducer);
   const [heading, setHeading] = useState('Coach Payout Details');
 
   const [monthDate, setMonthDate] =useState('');
@@ -49,6 +81,41 @@ function CoachPayoutDetails(props) {
         Get my DATA!
       </Button>
       <h2>{heading}</h2>
+      <br></br>
+      <br></br>
+      <TableContainer component={Paper} className={classes.table}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Client Name</TableCell>
+                <TableCell>Scheduled Date</TableCell>
+                <TableCell>Payment Status</TableCell>
+                <TableCell>Amount</TableCell>
+                <TableCell>Total Paid</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {payments.map((payment) => (
+                <TableRow key={payment.id} 
+                // onClick={() => handleRowClick(payment.confirmation_number)} 
+                className={classes.tableRow}>
+                  <TableCell>{payment.full_name}</TableCell>
+                  <TableCell>{payment.scheduled_date}</TableCell>
+                  <TableCell>{payment.payment_status}</TableCell>
+                  <TableCell>{payment.amount}</TableCell>
+                  <TableCell>{payment.total_paid}</TableCell>
+                  <TableCell><Button 
+                    variant="contained"
+                    color="primary"
+                    // onClick={() => handleDetails(payment.payout_date)}
+                    >Details</Button></TableCell>
+                  
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
     </div>
   );
 }
